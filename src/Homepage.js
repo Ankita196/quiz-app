d1959c4bbf2d3004fe0631c1cd4f7ec6f5d92c88
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-10-14 21:18:13 +0530

    done

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,27 +3,37 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import {Link} from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    marginLeft:300,
+    marginTop:100
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
     backgroundColor: '#3f51b5',
+ 
+    width:800,
+    marginLeft:100
   },
   paper1: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
     backgroundColor: '#009688',
+    
   },
   name: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 15,
   },
+  link:{
+    textDecoration:"none"
+  }
 }));
 
 export default function HomePage() {
@@ -40,19 +50,19 @@ export default function HomePage() {
           </Paper>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper1}>
+          <Link to="/easy" className={classes.link}><Paper className={classes.paper1}>
             <Typography className={classes.name}>Easy</Typography>
-          </Paper>
+          </Paper></Link>
         </Grid>{' '}
         <Grid item xs={3}>
-          <Paper className={classes.paper1}>
+        <Link to="/medium" className={classes.link}> <Paper className={classes.paper1}>
             <Typography className={classes.name}>Medium</Typography>
-          </Paper>
+          </Paper></Link>
         </Grid>{' '}
         <Grid item xs={3}>
-          <Paper className={classes.paper1}>
+        <Link to="/difficult" className={classes.link}> <Paper className={classes.paper1}>
             <Typography className={classes.name}>Hard</Typography>
-          </Paper>
+          </Paper></Link>
         </Grid>
       </Grid>
     </div>
