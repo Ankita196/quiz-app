da41b6b614168c14c64630ee1363f0bf11dc0713
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-10-14 21:19:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./style.css";
 import Homepage from "./Homepage"
+import Quiz from "./quiz"
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 export default function App() {
   return (
     <div>
      <BrowserRouter>
-      
+    
       <Switch>
       <Route  exact path='/'> <Homepage /> </Route>
-      {/* <Route path="/quiz" component={Quiz} /> 
-      */}
+       <Route path="/easy"  ><Quiz /> </Route>
+       <Route path="/medium"  ><Quiz /> </Route> 
+       <Route path="/difficult"  ><Quiz /> </Route>
+     
         
         </Switch>
       </BrowserRouter>
